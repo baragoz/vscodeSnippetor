cecda3bd1d6f54f9e076e6eaf63b1dd6534ab0de
baragoz <v24pupkin@gmail.com>
2026-05-09 13:29:52 -0400

    Make it possible to dnd files

--- media/js/DragAndDropHandler.js
+++ media/js/DragAndDropHandler.js
@@ -18,16 +18,12 @@ class DragAndDropHandler {
         };
 
         wrapper.ondragover = (e) => {
-            if (nodeItem.isFolder) {
-                e.preventDefault();
-                wrapper.style.backgroundColor = 'var(--vscode-list-hoverBackground)';
-            }
+            e.preventDefault();
+            wrapper.style.backgroundColor = 'var(--vscode-list-hoverBackground)';
         };
 
         wrapper.ondragleave = () => {
-            if (nodeItem.isFolder) {
-                wrapper.style.backgroundColor = '';
-            }
+            wrapper.style.backgroundColor = '';
         };
 
         wrapper.ondrop = (e) => {
@@ -41,11 +37,7 @@ class DragAndDropHandler {
         const dragData = e.dataTransfer.getData('text/plain').split(":");
         const sourcePath = dragData[1];
         const isMovingFolder = dragData[0] === "folder";
-        const targetPath = targetNodeItem.isFolder 
-            ? targetNodeItem.fullPath 
-            : targetNodeItem.fullPath.substring(0, targetNodeItem.fullPath.lastIndexOf('/'));
-
-        // Delegate to command handler's handlePasteCommand with 'move' command
-        this.commandHandler.handlePasteCommand('move', sourcePath, isMovingFolder, targetPath, targetNodeItem.isFolder);
+        this.commandHandler.handlePasteCommand('move', sourcePath, isMovingFolder,
+            targetNodeItem.fullPath, targetNodeItem.isFolder);
     }
 }
